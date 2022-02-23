a2d28a3c3c8f510e5d3699c73eff9df564ed5813
Artem Derbin <artemsdft@mail.ru>
2022-02-23 10:58:54 +0300

    Initial Commit

---
+++ Task 10 git/Task_10_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_10_gitApp.swift
+//  Task 10 git
+//
+//  Created by Артем Дербин on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Task_10_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
